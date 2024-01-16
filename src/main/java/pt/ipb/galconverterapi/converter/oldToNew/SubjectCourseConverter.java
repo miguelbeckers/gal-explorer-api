dd4dc32643146fe7b6d78a502f849dfbbccedd44
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2024-01-16 13:38:47 +0000

    implemented SubjectCourseConverter

--- src/main/java/pt/ipb/galconverterapi/converter/oldToNew/SubjectCourseConverter.java
+++ src/main/java/pt/ipb/galconverterapi/converter/oldToNew/SubjectCourseConverter.java
@@ -1,8 +1,48 @@
 package pt.ipb.galconverterapi.converter.oldToNew;
 
+import jakarta.persistence.Column;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import pt.ipb.galconverterapi.model._new.Subject;
+import pt.ipb.galconverterapi.model._new.SubjectCourse;
+import pt.ipb.galconverterapi.model.old.DisciplinaCurso;
+import pt.ipb.galconverterapi.repository.old.DisciplinaCursoRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class SubjectCourseConverter {
+    @Autowired
+    private DisciplinaCursoRepository disciplinaCursoRepository;
 
+    @Autowired
+    private SubjectConverter subjectConverter;
+
+    public List<SubjectCourse> convert() {
+        List<DisciplinaCurso> disciplinaCursos = disciplinaCursoRepository.findAll();
+        List<SubjectCourse> subjectCourses = new ArrayList<>();
+
+        List<Subject> subjects = subjectConverter.convert();
+
+        for (DisciplinaCurso disciplinaCurso : disciplinaCursos) {
+            SubjectCourse subjectCourse = new SubjectCourse();
+            subjectCourse.setId((long) disciplinaCurso.getId());
+
+            subjectCourse.setIdCurso(disciplinaCurso.getIdCurso());
+            subjectCourse.setIdAno(disciplinaCurso.getIdAno());
+            subjectCourse.setNumAlunos(disciplinaCurso.getNumAlunos());
+
+            subjectCourse.setSubject(subjects.stream()
+                    .filter(subject -> subject.getId() == disciplinaCurso.getIdDiscip())
+                    .findFirst()
+                    .orElseThrow(
+                            () -> new RuntimeException("Subject not found for id: " + disciplinaCurso.getIdDiscip())
+                    ));
+
+            subjectCourses.add(subjectCourse);
+        }
+
+        return subjectCourses;
+    }
 }
--- src/main/java/pt/ipb/galconverterapi/model/_new/SubjectCourse.java
+++ src/main/java/pt/ipb/galconverterapi/model/_new/SubjectCourse.java
@@ -7,4 +7,8 @@ public class SubjectCourse {
     private Long id;
     private Course course;
     private Subject subject;
+
+    private int idCurso;
+    private int idAno;
+    private int numAlunos;
 }
