3b71aeb31c39f2963616f6f776ebe91d364ac13e
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2024-01-18 22:48:33 +0000

    removed unnecessary models and repositories

--- src/main/java/pt/ipb/galconverterapi/repository/CheckpointRepository.java
+++
@@ -1,9 +0,0 @@
-package pt.ipb.galconverterapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pt.ipb.galconverterapi.model.Checkpoint;
-
-@Repository
-public interface CheckpointRepository extends JpaRepository<Checkpoint, Integer> {
-}
--- src/main/java/pt/ipb/galconverterapi/repository/EpocaExameRepository.java
+++
@@ -1,9 +0,0 @@
-package pt.ipb.galconverterapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pt.ipb.galconverterapi.model.EpocaExame;
-
-@Repository
-public interface EpocaExameRepository extends JpaRepository<EpocaExame, Integer> {
-}
