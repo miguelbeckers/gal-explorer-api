b4e46aa8aac936a031ab4a4a578449a217f0c5b6
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2024-01-18 22:07:03 +0000

    implemented lesson resource conversion for lessons

--- src/main/java/pt/ipb/galconverterapi/converter/oldToNew/LessonConverter.java
+++ src/main/java/pt/ipb/galconverterapi/converter/oldToNew/LessonConverter.java
@@ -6,9 +6,11 @@ import pt.ipb.galconverterapi.model._new.Lesson;
 import pt.ipb.galconverterapi.model.old.AulaDocente;
 import pt.ipb.galconverterapi.model.old.DetalhesAula;
 import pt.ipb.galconverterapi.model.old.DisciplinaCurso;
+import pt.ipb.galconverterapi.model.old.RecursoDisciplina;
 import pt.ipb.galconverterapi.repository.old.AulaDocenteRepository;
 import pt.ipb.galconverterapi.repository.old.DetalhesAulaRepository;
 import pt.ipb.galconverterapi.repository.old.DisciplinaCursoRepository;
+import pt.ipb.galconverterapi.repository.old.RecursoDisciplinaRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,10 +26,14 @@ public class LessonConverter {
     @Autowired
     private AulaDocenteRepository aulaDocenteRepository;
 
+    @Autowired
+    private RecursoDisciplinaRepository recursoDisciplinaRepository;
+
     public List<Lesson> convert() {
         List<DetalhesAula> detalhesAulas = detalhesAulaRepository.findAll();
         List<AulaDocente> aulaDocentes = aulaDocenteRepository.findAll();
         List<DisciplinaCurso> disciplinaCursos = disciplinaCursoRepository.findAll();
+        List<RecursoDisciplina> recursoDisciplinas = recursoDisciplinaRepository.findAll();
 
         List<Lesson> lessons = new ArrayList<>();
         for (DetalhesAula detalhesAula : detalhesAulas) {
@@ -41,13 +47,16 @@ public class LessonConverter {
                     lesson.setSubjectCourse((long) disciplinaCurso.getId());
                     lesson.setSubjectType((long) detalhesAula.getIdTipoAula());
 
-                    List<AulaDocente> aulaDocentesDetalheAula = aulaDocentes.stream()
-                            .filter(aulaDocente -> aulaDocente.getIdAula() == detalhesAula.getIdAula()).toList();
+                    lesson.setProfessors(aulaDocentes.stream()
+                            .filter(aulaDocente -> aulaDocente.getIdAula() == detalhesAula.getIdAula())
+                            .map(aulaDocente -> (long) aulaDocente.getIdDocente())
+                            .toList());
 
-                    lesson.setProfessors(aulaDocentesDetalheAula.stream()
-                            .map(aulaDocente -> (long) aulaDocente.getIdDocente()).toList());
+                    lesson.setLessonResources(recursoDisciplinas.stream()
+                            .filter(recursoDisciplina -> recursoDisciplina.getIdDiscip() == detalhesAula.getIdDiscip())
+                            .map(recursoDisciplina -> (long) recursoDisciplina.getId())
+                            .toList());
 
-                    //TODO: insert lesson resources
                     //TODO: insert lesson students
                     lessons.add(lesson);
                 }
