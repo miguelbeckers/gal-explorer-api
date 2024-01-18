84787f3f76175cfe8aa000f0f09c513e5000ccbc
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2024-01-18 23:37:19 +0000

    implementing HorarioConverter

--- src/main/java/pt/ipb/galconverterapi/converter/HorarioConverter.java
+++ src/main/java/pt/ipb/galconverterapi/converter/HorarioConverter.java
@@ -1,16 +1,22 @@
 package pt.ipb.galconverterapi.converter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pt.ipb.galconverterapi.dto.LessonDto;
 import pt.ipb.galconverterapi.model.Horario;
+import pt.ipb.galconverterapi.repository.HorarioRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class HorarioConverter {
+    @Autowired
+    private HorarioRepository horarioRepository;
+
     public List<Horario> convert(List<LessonDto> lessonDtos) {
         System.out.println("horarios converter. LessonDto.size(): " + lessonDtos.size());
+        horarioRepository.deleteAll();
         return new ArrayList<>();
     }
 }
