ae1bffdc957090e30221d74105ed7d2133119e7b
Miguel Beckers <128187386+miguel57403@users.noreply.github.com>
2024-01-20 00:51:56 +0000

    removed CursoEpoca

--- src/main/java/pt/ipb/galconverterapi/repository/CursoEpocaRepository.java
+++
@@ -1,9 +0,0 @@
-package pt.ipb.galconverterapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pt.ipb.galconverterapi.model.CursoEpoca;
-
-@Repository
-public interface CursoEpocaRepository extends JpaRepository<CursoEpoca, Integer> {
-}
