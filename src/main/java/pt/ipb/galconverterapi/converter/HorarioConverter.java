bdce034b19fc016d497ee71417a47ba27e2bb368
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2024-01-25 18:54:47 +0000

    implemented horarios merging

--- src/main/java/pt/ipb/galconverterapi/converter/HorarioConverter.java
+++ src/main/java/pt/ipb/galconverterapi/converter/HorarioConverter.java
@@ -9,6 +9,7 @@ import pt.ipb.galconverterapi.repository.HorarioRepository;
 
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
@@ -33,18 +34,41 @@ public class HorarioConverter {
         }
 
         List<Horario> horarios = new ArrayList<>();
+        int id = 1;
 
         for (LessonUnitDto lessonDto : lessonUntiDtos) {
             if (lessonDto.getTimeslotId() != null && lessonDto.getLessonId() != null) {
                 TimeslotDto timeslotDto = timeslotDtoHashMap.get(lessonDto.getTimeslotId());
 
                 Horario horario = new Horario();
+                horario.setId(id++);
                 horario.setIdSala(lessonDto.getClassroomId().intValue());
                 horario.setInicio(Time.valueOf(timeslotDto.getStartTime()));
                 horario.setFim(Time.valueOf(timeslotDto.getEndTime()));
                 horario.setIdDia(timeslotDto.getDayOfWeek().getValue());
                 horario.setIdAula(lessonDto.getLessonId().intValue());
                 horarios.add(horario);
+            }
+        }
+
+        horarios.sort(Comparator.comparing(Horario::getInicio));
+        horarios.sort(Comparator.comparing(Horario::getIdDia));
+        horarios.sort(Comparator.comparing(Horario::getIdSala));
+        horarios.sort(Comparator.comparing(Horario::getIdAula));
+
+        int i = 0;
+
+        while (i < horarios.size() - 1) {
+            Horario horario = horarios.get(i);
+            Horario nextHorario = horarios.get(i + 1);
+
+            if (horario.getIdAula() == nextHorario.getIdAula()
+                    && horario.getIdDia() == nextHorario.getIdDia()
+                    && horario.getIdSala() == nextHorario.getIdSala()) {
+                horarios.remove(i + 1);
+                horario.setFim(nextHorario.getFim());
+            } else {
+                i++;
             }
         }
 
